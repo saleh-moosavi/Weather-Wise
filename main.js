85cb17e9187c4d3a73a991f15bda1e22629ae66f
Saleh Moosavi <salehmoosavi77@gmail.com>
2024-12-25 23:24:42 +0330

    refactor:js codes refactored and add loader

--- main.js
+++ main.js
@@ -1,56 +1,79 @@
-const form = document.getElementById("weather-form")
-const formInput = document.getElementById("weather-input")
-const weatherDetail = document.getElementById("weather-detail")
+const form = document.getElementById("weather-form");
+const formInput = document.getElementById("weather-input");
+const weatherDetail = document.getElementById("weather-detail");
 
-//fetch data
 const apiKey = "35260a6010a72194c616b209e339db17";
+
+// Fetch weather data by location
 async function getWeatherByLocation(location) {
-    weatherDetail.innerHTML = "*"
-    weatherDetail.classList.remove("hidden")
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`)
-    const response = await data.json()
-    parseInt(response.cod) == 200 && addWeatherInfoToPage(response)
+    showLoader();
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`);
+        const data = await response.json();
+        if (response.ok) {
+            displayWeatherInfo(data);
+        } else {
+            displayError("City not found");
+        }
+    } catch (error) {
+        displayError("Error fetching data");
+    }
 }
 
-const hideDetailInfo = () => {
-    weatherDetail.innerHTML = ""
-    weatherDetail.classList.add("hidden")
+// Show loader while fetching data
+function showLoader() {
+    weatherDetail.innerHTML = '<div class="custom-loader"></div>';
+    weatherDetail.classList.remove("hidden");
 }
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    Boolean(formInput.value) ? getWeatherByLocation(formInput.value) : hideDetailInfo()
-})
-formInput.addEventListener("input", (e) => {
-    e.preventDefault()
-    Boolean(formInput.value) ? getWeatherByLocation(formInput.value) : hideDetailInfo()
-})
+// Hide weather details
+function hideDetailInfo() {
+    weatherDetail.innerHTML = "";
+    weatherDetail.classList.add("hidden");
+}
 
+// Display weather information on the page
+function displayWeatherInfo(data) {
+    const temperature = convertKelvinToCelsius(data.main.temp);
+    const maxTemp = convertKelvinToCelsius(data.main.temp_max);
+    const minTemp = convertKelvinToCelsius(data.main.temp_min);
 
-function addWeatherInfoToPage(data) {
-    const temperature = Kelvin_To_Centigrade(data.main.temp)
-    const maxTemp = Kelvin_To_Centigrade(data.main.temp_max)
-    const minTemp = Kelvin_To_Centigrade(data.main.temp_min)
-    console.log(data)
-
-    weatherDetail.innerHTML = ""
-    weatherDetail.innerHTML =
-        `<div>
-          <h2 class="weather-detail-bolds">${data.sys.country} - ${data.name}</h2>
-          <p class="weather-detail-normals">sunday 22 Nov 2020</p>
+    weatherDetail.innerHTML = `
+        <div>
+            <h2 class="weather-detail-bolds">${data.sys.country} - ${data.name}</h2>
+            <p class="weather-detail-normals">sunday 22 Nov 2020</p>
         </div>
         <div>
             <h1 class="weather-detail-bolds">${temperature}°C</h1>
-            <p class="weather-detail-normals">Sky : ${data.weather[0].description}</p>
+            <p class="weather-detail-normals">Sky: ${data.weather[0].description}</p>
         </div>
         <div class="weather-detail-normals">
-          <p>Min Temp : ${minTemp}°C</p>
-          <p>Max Temp : ${maxTemp}°C</p>
-          <p>Wind Speed : ${data.wind.speed}</p>
-        </div>`
-    weatherDetail.classList.remove("hidden")
+            <p>Min Temp: ${minTemp}°C</p>
+            <p>Max Temp: ${maxTemp}°C</p>
+            <p>Wind Speed: ${data.wind.speed}</p>
+        </div>`;
+    weatherDetail.classList.remove("hidden");
 }
 
-function Kelvin_To_Centigrade(temp) {
-    return parseInt(temp - 273.15)
+// Display error message
+function displayError(message) {
+    weatherDetail.innerHTML = message;
 }
+
+// Convert Kelvin to Celsius
+function convertKelvinToCelsius(temp) {
+    return Math.round(temp - 273.15);
+}
+
+// Event listeners for form submission and input
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const location = formInput.value.trim();
+    location ? getWeatherByLocation(location) : hideDetailInfo();
+});
+
+formInput.addEventListener("input", (e) => {
+    e.preventDefault();
+    const location = formInput.value.trim();
+    location ? getWeatherByLocation(location) : hideDetailInfo();
+});
